ab24588851c39ba752e5daf63e8b7fdaea63a7fd
santaceciliahustler <rph.costa1993@gmail.com>
2022-04-09 19:55:56 -0300

    aula encontro remoto 2

---
+++ Raphael_de_Oliveira_Costa_contagem_aluno.js
@@ -0,0 +1,15 @@
+let numeroDeAlunos = 10
+let contador = 0
+
+
+        for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+    
+    if (contador===0) {
+        console.log("O número atual é ZERO")
+        
+    }else if (contador % 2 == 0) 
+    {console.log("o número "+contador+ " é PAR ")
+    } else {console.log("O número "+ contador+ " é IMPAR")
+        
+    }
+}
